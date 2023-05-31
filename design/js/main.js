6c5956e7f924f2e5f31fb3a05342869d6f5df625
alb91 <alb9112@gmail.com>
2023-05-30 20:41:51 -0600

    added functionality to main card buttons

--- design/js/main.js
+++ design/js/main.js
@@ -1,7 +1,10 @@
 const mainCard = document.getElementById("mainCard");
 const secondaryCard = document.getElementById("secondaryCard");
-
 const ratingItems = document.querySelectorAll(".rating li");
+const submitSelection = document.getElementById(".submitSelection");
+
+// Keep color active when clicked on
+let selectedRatingItem = null;
 
 // Hiding second card
 function hideSecondaryCard() {
@@ -22,4 +25,17 @@ ratingItems.forEach(function (item) {
     this.style.backgroundColor = "";
     this.style.color = "";
   });
+
+  //Click function for rating numbers
+  item.addEventListener("click", function () {
+    // Add styles to the click Item
+    this.style.backgroundColor = "hsl(25, 97%, 53%)";
+    this.style.color = "hsl(0, 0%, 100%)";
+  });
+});
+
+//Click function for submit button
+submitSelection.addEventListener("click", function () {
+  this.style.backgroundColor = "hsl(0, 0%, 100%)";
+  this.stylecolor = "hsl(25, 97%, 53%)";
 });
